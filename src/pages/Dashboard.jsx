6e946ce7a412d380b7ffa57d84301f71dc00ad28
Kamya Tanhaie <dgghost034@gmail.com>
2026-05-04 17:27:26 +0330

    move stats to storage and used mapping

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,38 @@ import Sidebar from "../components/layout/Sidebar/Sidebar";
 import CompletionChart from "../features/dashboard/CompletionChart";
 import StatsCards from "../features/dashboard/StatsCards";
 import style from "./Dashboard.module.css";
-import { ClipboardList, Timer, CircleAlert, CircleCheck } from "lucide-react";
+import { ClipboardList, Timer, CircleAlert, CircleCheck, icons } from "lucide-react";
+
+const stats = [
+  {
+    title: "Total Tasks",
+    icon: <ClipboardList color="var(--primary)" size={38} />,
+    value: 24,
+    change: 12,
+    backColor: "var(--info-light)",
+  },
+  {
+    title: "In Progress",
+    icon: <Timer size={38} color={"var(--warning)"} />,
+    value: 8,
+    change: 5,
+    backColor: "var(--warning-light)",
+  },
+  {
+    title: "Completed",
+    icon: <CircleCheck size={38} color="var(--success)" />,
+    value: 12,
+    change: 18,
+    backColor: "var(--info-light)",
+  },
+  {
+    title: "Overdue",
+    icon: <CircleAlert size={38} color="var(--danger)" />,
+    value: 4,
+    change: 8,
+    backColor: "var(--info-light)",
+  },
+];
 
 const Dashboard = () => {
   return (
@@ -13,34 +44,17 @@ const Dashboard = () => {
         <Header />
         <div className="d-flex w-100  mt-4">
           <div className="d-flex">
-            <StatsCards
-              icon={<ClipboardList size={38} color="var(--primary)" />}
-              title={"Total Tasks"}
-              value={24}
-              change={12}
-              backColor={"var(--info-light)"}
-            />
-            <StatsCards
-              icon={<Timer size={38} color={"var(--warning)"} />}
-              title={"In Progress"}
-              value={8}
-              change={5}
-              backColor={"var(--warning-light)"}
-            />
-            <StatsCards
-              icon={<CircleCheck size={38} color="var(--success)" />}
-              title={"Completed"}
-              value={12}
-              change={18}
-              backColor={"var(--success-light)"}
-            />
-            <StatsCards
-              icon={<CircleAlert size={38} color="var(--danger)" />}
-              title={"Overdue"}
-              value={4}
-              change={8}
-              backColor={"var(--danger-light)"}
-            />
+            {stats.map(({ icon, title, value, change, backColor }) => {
+              return (
+                <StatsCards
+                  icon={icon}
+                  title={title}
+                  value={value}
+                  change={change}
+                  backColor={backColor}
+                />
+              );
+            })}
           </div>
           <div className="w-100">
             <CompletionChart />
