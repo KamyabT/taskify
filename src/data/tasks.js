d33ab155abb4f34b50d3b93dd4a81b0c84e951b8
Kamya Tanhaie <dgghost034@gmail.com>
2026-05-14 13:08:48 +0330

    mock data for tasks created, added to priority chart and made it dynamic at the moment

---
+++ src/data/tasks.js
@@ -0,0 +1,52 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Design dashboard",
+    description: "Create a responsive dashboard design for the new project.",
+    dueDate: "2026-05-15",
+    priority: "Medium",
+    status: "in-progress",
+    color: "#f59e0b",
+  },
+  {
+    id: 2,
+    title: "Build Database",
+    description:
+      "Create a database schema and implement the necessary tables for the new project.",
+    dueDate: "2026-07-01",
+    priority: "High",
+    status: "Completed",
+    color: "#ff4d6d",
+  },
+  {
+    id: 3,
+    title: "UI/UX Design",
+    description:
+      "Design the user interface and user experience for the new project, ensuring it is intuitive and user-friendly.",
+    dueDate: "2026-07-15",
+    priority: "Low",
+    status: "To Do",
+    color: "#22c55e",
+  },
+  {
+    id: 4,
+    title: "MVP Development",
+    description:
+      "Release an MVP version of the project to gather user feedback and validate the concept.",
+    dueDate: "2026-05-13",
+    priority: "Medium",
+    status: "Overdue",
+    color: "#f59e0b",
+  },
+  {
+    id: 5,
+    title: "Backend API",
+    description: "Create a basis for the backend API to support the new project.",
+    dueDate: "2026-05-20",
+    priority: "High",
+    status: "Completed",
+    color: "#ff4d6d",
+  },
+];
+
+export default tasks;
--- src/features/dashboard/PriorityChart.jsx
+++ src/features/dashboard/PriorityChart.jsx
@@ -1,17 +1,29 @@
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import tasks from "../../data/tasks";
 
 import style from "./PriorityChart.module.css";
 
-const data = [
-  { name: "High", value: 8, color: "#ff4d6d" },
-  { name: "Medium", value: 10, color: "#f59e0b" },
-  { name: "Low", value: 6, color: "#22c55e" },
-];
-
-const totalValue = data.reduce((acc, item) => acc + item.value, 0);
-console.log("Total Value:", totalValue);
-
 const PriorityChart = () => {
+  const groupedData = [
+    {
+      priority: "High",
+      value: tasks.filter((task) => task.priority === "High").length,
+      color: "#ff4d6d",
+    },
+    {
+      priority: "Medium",
+      value: tasks.filter((task) => task.priority === "Medium").length,
+      color: "#f59e0b",
+    },
+    {
+      priority: "Low",
+      value: tasks.filter((task) => task.priority === "Low").length,
+      color: "#22c55e",
+    },
+  ];
+
+  const totalValue = groupedData.reduce((acc, item) => acc + item.value, 0);
+
   return (
     <div className={style.chartCard}>
       <h5>Tasks Overview</h5>
@@ -21,26 +33,26 @@ const PriorityChart = () => {
           <ResponsiveContainer width="100%" height={220}>
             <PieChart>
               <Pie
-                data={data}
+                data={groupedData}
                 innerRadius={60}
                 outerRadius={80}
                 dataKey="value"
                 paddingAngle={3}
               >
-                {data.map((item) => (
-                  <Cell key={item.name} fill={item.color} />
+                {groupedData.map((item) => (
+                  <Cell key={item.priority} fill={item.color} />
                 ))}
               </Pie>
             </PieChart>
           </ResponsiveContainer>
         </div>
         <div className={style.legend}>
-          {data.map((item) => (
-            <div key={item.name} className={style.legendItem}>
+          {groupedData.map((item) => (
+            <div key={item.priority} className={style.legendItem}>
               <div className="d-flex align-items-center">
                 <div className={style.color} style={{ backgroundColor: item.color }} />
 
-                <span className="text-card-value-small ms-2">{item.name}</span>
+                <span className="text-card-value-small ms-2">{item.priority}</span>
               </div>
 
               <span className="text-card-value-small">
