4ff87637e34bf7d970c62b82ddd2006eca5c05c0
Kamya Tanhaie <dgghost034@gmail.com>
2026-05-26 02:16:10 +0330

    working on changing task status in table

--- src/features/tasks/components/TaskRow.jsx
+++ src/features/tasks/components/TaskRow.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Button from "../../../components/ui/Button";
 import taskStyles from "../../../utils/taskStyles";
 import style from "./TaskRow.module.css";
-import { SquarePen, Trash2 } from "lucide-react";
+import { SquarePen, Trash2, CircleCheck } from "lucide-react";
 import { format, formatDistance } from "date-fns";
 // import { format, formatDistance } from "date-fns-jalali";
 // import { faIR } from "date-fns-jalali/locale";
 
 const TaskRow = ({ task, handleDelete }) => {
+  const [isEditing, setIsEditing] = useState(false);
   return (
     <div className={`${style.row}`}>
       <div className="d-flex align-items-center">
@@ -29,9 +31,29 @@ const TaskRow = ({ task, handleDelete }) => {
         </span>
       </div>
       <div className="">
-        <span className={`${style.badge} ${style[taskStyles.status[task.status]]}`}>
-          {task.status}
-        </span>
+        {isEditing ? (
+          <div className="d-flex align-items-center">
+            <select className={`${style.select}`}>
+              <option value="To Do">To Do</option>
+              <option value="In-Progress">In-Progress</option>
+              <option value="Completed">Completed</option>
+              <option value="Overdue">Overdue</option>
+            </select>
+            <span className="d-flex align-items-center ms-1">
+              <CircleCheck size={22} color="var(--success)" />
+            </span>
+            {/* <span className="d-flex align-items-center ms-1">
+              <CircleCheck size={22} color="var(--success)" />
+            </span> */}
+          </div>
+        ) : (
+          <span
+            className={`${style.badge} ${style[taskStyles.status[task.status]]} ${style.cursorPointer}`}
+            onClick={() => setIsEditing(!isEditing)}
+          >
+            {task.status}
+          </span>
+        )}
       </div>
       <div className="">
         <span className={`${style.date}`}>{format(task.dueDate, "dd/MM/yyyy")}</span>
@@ -40,13 +62,10 @@ const TaskRow = ({ task, handleDelete }) => {
         <span className={`${style.date}`}>{format(task.dueDate, "dd/MM/yyyy")}</span>
       </div>
       <div className="">
-        <span
-          className={`${style.date}`}
-          
-        >
+        <span className={`${style.date}`}>
           {formatDistance(new Date(task.dueDate), new Date(), {
-              addSuffix: true,
-            })}
+            addSuffix: true,
+          })}
         </span>
       </div>
       <div className="">
