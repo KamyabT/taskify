91cb4b08cfd450fbdd18a09948be6a73e0069096
Kamya Tanhaie <dgghost034@gmail.com>
2026-06-01 01:59:55 +0330

    adding a text for empty task list

--- src/features/tasks/components/TasksTable.jsx
+++ src/features/tasks/components/TasksTable.jsx
@@ -3,13 +3,23 @@ import TableBody from "./TableBody";
 import { useTaskFilters } from "../../../hooks/useTaskFilters";
 import style from "./TasksTable.module.css";
 
-const TasksTable = ({ tasks , handleDelete , isLoading }) => {
+const TasksTable = ({ tasks, handleDelete, isLoading }) => {
   const { filters, dispatch, filteredTasks } = useTaskFilters(tasks);
 
   return (
     <section className={`${style.tasksTableWrapper} mt-4`}>
       <TableFilters filters={filters} dispatch={dispatch} />
-      <TableBody tasks={filteredTasks} handleDelete={handleDelete} isLoading={isLoading} />
+      {tasks.length <= 0 ? (
+        <p className="text-center text-card-value-medium py-3">
+          No tasks found. Create your first task
+        </p>
+      ) : (
+        <TableBody
+          tasks={filteredTasks}
+          handleDelete={handleDelete}
+          isLoading={isLoading}
+        />
+      )}
     </section>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -129,7 +129,7 @@ const Dashboard = () => {
         </div>
         <div className={`${style.charts} mt-4`}>
           <OverViewChart />
-          <PriorityChart data={tasks}/>
+          <PriorityChart data={tasks} />
         </div>
         <div>
           <TasksTable tasks={tasks} isLoading={isLoading} handleDelete={handleDelete} />
